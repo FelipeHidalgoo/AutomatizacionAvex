12823dd5c990713e244ae131edeee26f53834360
FelipeHidalgoo <felipehm09@gmail.com>
2024-04-21 22:16:41 -0400

    Modificacion PIN RT/Cliente

---
+++ src/main/java/valuesite/pageobjects/ModificacionPinCliente.java
@@ -0,0 +1,92 @@
+package valuesite.pageobjects;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import valuesite.componentesreusables.ComponentesReusables;
+
+public class ModificacionPinCliente extends ComponentesReusables {
+
+	WebDriver driver;
+
+	// Incializa driver y PageFactory
+	public ModificacionPinCliente(WebDriver driver) {
+
+		super(driver);
+
+		this.driver = driver;
+
+		PageFactory.initElements(driver, this);
+	}
+
+	// PAGE FACTORY
+
+	// Pestaña de adminstracion en navBar
+	@FindBy(xpath = "//a[normalize-space()='Administración']")
+	WebElement tabAdministracion;
+
+	// Pestaña de area cliente en navBar
+	@FindBy(xpath = "//a[normalize-space()='Modificación PIN']")
+	WebElement tabModificacionPin;
+
+	@FindBy(id = "btnLimpiarPin")
+	public WebElement btnCancelar;
+
+	@FindBy(id = "btnActualizaPin")
+	public WebElement btnActualizar;
+
+	@FindBy(id = "txtPin")
+	public WebElement campoPin;
+
+	@FindBy(css = ".alert.alert-danger")
+	public WebElement msjError;
+
+	@FindBy(css = ".alert.alert-success")
+	public WebElement msjExito;
+
+	// PAGE FACTORY
+
+	public void ingresoMantenedorPin() {
+		tabAdministracion.click();
+		tabModificacionPin.click();
+	}
+
+	public void ingresaPin(String pin) {
+		campoPin.sendKeys(pin);
+	}
+
+	public void actualizaPin(String pin) {
+		campoPin.sendKeys(pin);
+		btnActualizar.click();
+	}
+
+	public String obtienePIN() {
+		String pin = campoPin.getAttribute("value");
+
+		// Decodificar caracteres especiales si es necesario
+		pin = decodeSpecialCharacters(pin);
+
+		return pin;
+	}
+
+	// Decodifica caracteres especiales para poder retornalos como texto
+	private String decodeSpecialCharacters(String input) {
+		input = input.replaceAll("&amp;", "&").replaceAll("&lt;", "<").replaceAll("&gt;", ">")
+				.replaceAll("&quot;", "\"").replaceAll("&apos;", "'").replaceAll("&#x21;", "!")
+				.replaceAll("&#x22;", "\"").replaceAll("&#x23;", "#").replaceAll("&#x24;", "$")
+				.replaceAll("&#x25;", "%").replaceAll("&#x26;", "&").replaceAll("&#x27;", "'").replaceAll("&#x28;", "(")
+				.replaceAll("&#x29;", ")").replaceAll("&#x2A;", "*").replaceAll("&#x2B;", "+").replaceAll("&#x2C;", ",")
+				.replaceAll("&#x2D;", "-").replaceAll("&#x2E;", ".").replaceAll("&#x2F;", "/").replaceAll("&#x3A;", ":")
+				.replaceAll("&#x3B;", ";").replaceAll("&#x3C;", "<").replaceAll("&#x3D;", "=").replaceAll("&#x3E;", ">")
+				.replaceAll("&#x3F;", "?").replaceAll("&#x40;", "@").replaceAll("&#x5B;", "[")
+				.replaceAll("&#x5C;", "\\").replaceAll("&#x5D;", "]").replaceAll("&#x5E;", "^")
+				.replaceAll("&#x5F;", "_").replaceAll("&#x60;", "`").replaceAll("&#x7B;", "{").replaceAll("&#x7C;", "|")
+				.replaceAll("&#x7D;", "}").replaceAll("&#x7E;", "~");
+
+		return input;
+	}
+
+}
---
+++ src/test/java/valuesite/tests/ModificacionPinTestCliente.java
@@ -0,0 +1,186 @@
+package valuesite.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import valuesite.componentesreusables.ActionsHelper;
+import valuesite.componentesreusables.ComponentesReusables;
+import valuesite.pageobjects.Login;
+import valuesite.pageobjects.ModificacionPinRT;
+import valuesite.testcomponents.BaseTest;
+
+public class ModificacionPinTestCliente extends BaseTest{
+	
+	ModificacionPinRT modifpinrt;
+	ComponentesReusables componentesReusables;
+	
+	@BeforeTest
+	public void setUp() {
+		modifpinrt = new ModificacionPinRT(driver);
+		componentesReusables = new ComponentesReusables(driver);
+    }
+    
+    @BeforeClass
+    public void ingresaMantenedor() {
+		Login login = new Login(driver);
+		
+		// Pasa como parametro correo y contraseña para ingresar a la web
+		login.iniciarSesion("userautocliente@aquivoy.cl", "123456");
+		
+		//AreaRT areart = new AreaRT(driver);
+		
+		//ModificacionPinRT modifpinrt = new ModificacionPinRT(driver);
+		modifpinrt.ingresoMantenedorPin();
+	}
+	
+	
+	@Test(priority=1)
+	public void vaciaCampo() {
+		componentesReusables.waitForWebElementToAppear(modifpinrt.campoPin);
+		
+		modifpinrt.ingresaPin("7546");
+		modifpinrt.campoPin.clear();
+		
+		String pin = modifpinrt.obtienePIN();
+		//System.out.println(pin);
+		
+		if (pin.isEmpty()) {
+			System.out.println("El campo se vacio correctamente\n");
+			Assert.assertTrue(true);
+		}else {
+			System.out.println("El campo NO se vacio correctamente\n");
+			Assert.assertTrue(false);
+		}
+	}
+	
+    @Test(priority=2)
+	public void ingresarPin() {
+		String pin = "7546";
+		componentesReusables.waitForWebElementToAppear(modifpinrt.campoPin);
+		
+		// Vacia el campo, ingresa un dato y guarda el dato que hay actualmente en el campo en "nuevoPin" 
+		//despues de ingresarlo
+		modifpinrt.campoPin.clear();
+		modifpinrt.ingresaPin(pin);
+		String nuevoPin = modifpinrt.obtienePIN();
+		
+		// Compara "nuevoPin" con "pin" para comprobar que el dato efectivamente se ingreso en el campo
+		if (nuevoPin.equals(pin)) {
+			System.out.println("El pin se ingreso correctamente");
+			Assert.assertTrue(true);
+		}else {
+			System.out.println("El pin no fue ingresado");
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test(priority=3)
+	public void actualizaPin() {
+		String pin = "9876";
+		componentesReusables.waitForWebElementToAppear(modifpinrt.campoPin);
+		
+		modifpinrt.campoPin.clear();
+		modifpinrt.actualizaPin(pin);
+		
+		if (modifpinrt.msjExito.isDisplayed()) {
+			System.out.println("El pin fue actualizado correctamente");
+			Assert.assertTrue(true);
+		}else {
+			System.out.println("El pin no fue actualizado");
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test(priority=4)
+	public void actualizaSinDatos() {
+		componentesReusables.waitForWebElementToAppear(modifpinrt.campoPin);
+		
+		modifpinrt.campoPin.clear();
+		modifpinrt.btnActualizar.click();
+		
+		if (modifpinrt.msjError.isDisplayed()) {
+			System.out.println("No permite actualizar sin ingresar datos");
+			Assert.assertTrue(true);
+		}else {
+			System.out.println("Permite actualizar sin ingresar datos");
+			Assert.assertTrue(false);
+		}
+	}
+	
+	
+	@Test (priority=5)
+	public void actualizaConCaracteresEspeciales() throws InterruptedException {
+		componentesReusables.waitForElementToDisappear(modifpinrt.msjError);
+		
+		String caracteresEspeciales = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+		componentesReusables.waitForWebElementToBeClickable(modifpinrt.campoPin);
+		
+		modifpinrt.campoPin.clear();
+		modifpinrt.actualizaPin(caracteresEspeciales);
+		
+		componentesReusables.waitForWebElementToAppear(modifpinrt.msjError);
+		if (modifpinrt.msjError.isDisplayed()) {
+			System.out.println("No permite actualizar con caracteres especiales");
+			Assert.assertTrue(true);
+		}else {
+			System.out.println("Permite actualizar con caracteres especiales");
+			Assert.assertTrue(false);
+		}
+	}
+	
+	@Test(priority=6)
+	public void limpiaCampo() {
+		modifpinrt.campoPin.clear();
+		modifpinrt.ingresaPin("5465");
+		
+		modifpinrt.btnCancelar.click();
+		
+		String pinActual = modifpinrt.obtienePIN();
+		if (pinActual.isEmpty()) {
+			System.out.println("Se limpio correctamente el campo PIN");
+			Assert.assertTrue(true);
+		}else {
+			System.out.println("El campo PIN no se limpio correctamente");
+			Assert.assertTrue(false);
+		}
+	}	
+
+    
+	 // Valida que no se puedan ingresar carcateres especiales en el campo pin
+	  @Test(priority=7)
+		public void ingresaCaracteresEspeciales() {
+			componentesReusables.waitForWebElementToAppear(modifpinrt.campoPin);
+			
+			String caracteresEspeciales = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+			
+			modifpinrt.campoPin.clear();
+			modifpinrt.ingresaPin(caracteresEspeciales);
+			
+			
+			String valorPin = modifpinrt.obtienePIN();
+			
+			
+			// Verificar si el campo contiene caracteres especiales
+		    boolean contieneCaracteresEspeciales = false;
+		    for (char c : caracteresEspeciales.toCharArray()) {
+		        if (valorPin.contains(String.valueOf(c))) {
+		            contieneCaracteresEspeciales = true;
+		            break;
+		        }
+		    }
+		    
+		    // Imprimir el resultado y realizar la aserción
+		    if (contieneCaracteresEspeciales) {
+		        System.out.println("Se ingresaron caracteres especiales en el campo \n");
+		        Assert.assertTrue(false);
+		    } else {
+		        System.out.println("No se ingresaron caracteres especiales en el campo \n");
+		        Assert.assertTrue(true);
+		    }	
+		}
+}
